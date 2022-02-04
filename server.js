c83a776302c302ed547b62e60f67add98f260005
unknown <lopez1432.al@gmail.com>
2022-02-03 22:22:18 -0800

    server is up and fixed minor errors in connection and login js

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const PORT = process.env.PORT || 5001
 
 const app = express();
 
+const handlebars = require('express-handlebars');
+const bcrypt = require('bcrypt');
+const mysql2 = require('mysql2');
+const sequelize = require('sequelize');
+
+app.use(express.static("public"));
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// const routes = require("./controllers/api");
+
+// app.use(routes);
+
+
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`)
 });
