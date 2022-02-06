609fab8dec1e60a169f6b6b4e6995b1479431bcd
Connor White <connorwhitepdx@gmail.com>
2022-02-05 17:36:52 -0800

    template and homepage wireframed

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const Handlebars = require("handlebars");
 const template = Handlebars.compile("Name: {{name}}");
 console.log(template({ name: "Nils" }));
 
+// 
+const path = require('path');
+
+
 const express = require('express');
 const axios = require('axios');
 const PORT = process.env.PORT || 5001
@@ -9,13 +13,21 @@ const PORT = process.env.PORT || 5001
 const app = express();
 
 const handlebars = require('express-handlebars');
+// added from module 14
+const hbs = handlebars.create({});
+
 const bcrypt = require('bcrypt');
 const mysql2 = require('mysql2');
 const sequelize = require('sequelize');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+// middleware for css 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// added from module 14
+app.engine('handlebars', hbs.engine);
+//app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // const routes = require("./controllers/api");
